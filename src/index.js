a25b22ebe74b2d78043bf676bbd3f4087e60ee00
Lucas Sweeney <lsweeney0124@gmail.com>
2022-08-08 11:40:07 -0600

    practice using Yup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useFormik } from "formik";
+import "./styles.css";
+import * as Yup from 'yup';
+ 
+ const SignupForm = () => {
+   const formik = useFormik({
+     initialValues: {
+       firstName: '',
+       lastName: '',
+       email: '',
+     },
+     validationSchema: Yup.object({
+       firstName: Yup.string()
+         .max(15, 'Must be 15 characters or less')
+         .required('Required'),
+       lastName: Yup.string()
+         .max(20, 'Must be 20 characters or less')
+         .required('Required'),
+       email: Yup.string().email('Invalid email address').required('Required'),
+     }),
+     onSubmit: values => {
+       alert(JSON.stringify(values, null, 2));
+     },
+   });
+   return (
+     <form onSubmit={formik.handleSubmit}>
+       <label htmlFor="firstName">First Name</label>
+       <input
+         id="firstName"
+         name="firstName"
+         type="text"
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.firstName}
+       />
+       {formik.touched.firstName && formik.errors.firstName ? (
+         <div>{formik.errors.firstName}</div>
+       ) : null}
+ 
+       <label htmlFor="lastName">Last Name</label>
+       <input
+         id="lastName"
+         name="lastName"
+         type="text"
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.lastName}
+       />
+       {formik.touched.lastName && formik.errors.lastName ? (
+         <div>{formik.errors.lastName}</div>
+       ) : null}
+ 
+       <label htmlFor="email">Email Address</label>
+       <input
+         id="email"
+         name="email"
+         type="email"
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.email}
+       />
+       {formik.touched.email && formik.errors.email ? (
+         <div>{formik.errors.email}</div>
+       ) : null}
+ 
+       <button type="submit">Submit</button>
+     </form>
+   );
+ };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return <SignupForm />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
